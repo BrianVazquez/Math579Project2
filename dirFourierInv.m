734a6702c3e3479650ed76e49278dd3762781684
Brian Vazquez <vazquez.brian.theory@gmail.com>
2018-05-06 01:34:43 -0700

    updated F.T

--- dirFourierInv.m
+++ dirFourierInv.m
@@ -25,17 +25,17 @@ L = t(end);
 [length_t, theta_k] = size(S);
 % f = zeros(length_t);
 Rf_hat = zeros(length_t,theta_k); % D.F.T of Rf/S
-
-j = -N:1:N;
-for n = -N:1:N
-    % Fix theta_k  for supp(Rf(t,w(theta))) subset of [-L,L]
-    for k = 1:theta_k
-        temp_vec = (-pi*n.*(j*L/N))/N;
-        temp_S = S(:,k).*complex(cos(temp_vec),sin(temp_vec))';
-        Rf_hat(:,k) = (L/N)*sum(temp_S);
+n = -N:1:N;
+for k = 1:theta_k
+    for rn = 1:length(n)
+        % Fix theta_k  for supp(Rf(t,w(theta))) subset of [-L,L]
+        temp_vec = (-n(rn)*pi/L)*t;
+        temp_S = S(:,k).*complex(cos(temp_vec),sin(temp_vec));
+        Rf_hat(rn,k) = (L/N)*sum(temp_S);
     end
 end
 
+% change of variable 
 
 
 f = Rf_hat;
